e4a3e1cd35bca464ab8f16b7a1afc477fd178560
Gonzalez32 <3232rose@gmail.com>
2022-01-25 15:25:24 -0600

    added user input

---
+++ Lesson_1/user_input.rb
@@ -0,0 +1,51 @@
+# Input
+
+=begin 
+    To get input from the user in Ruby, you use the gets method, 
+    which returns what the user types as a string. 
+    To store the input for later use, 
+    you can assign the return value to a variable.
+=end 
+
+# x = gets
+
+# puts x
+
+# gets gets a line of text, including the new line at the end. If you do not want to include the new line, use the gets.chomp method:
+
+# puts "Enter Your Name!"
+
+# puts "%%%%%%%%%%%%%%%%"
+
+# name = gets.chomp
+
+# puts "%%%%%%%%%%%%%%%%"
+
+# puts "Welcome, #{name}"
+
+# puts "%%%%%%%%%%%%%%%%"
+
+=begin 
+    The Value of the input is a string. To convert it to an integer,
+    you can use the gets.to_i method.
+=end
+
+#quiz
+
+# temp = gets
+# puts "You entered #{temp}"
+
+ice = "7"
+# because this is being added together with Strings.
+ice += "2"
+
+puts ice
+
+
+a = 4
+
+puts a**2
+
+# mutltiple by twice 4 * 2 = 8 * 2 = 16 
+
+
