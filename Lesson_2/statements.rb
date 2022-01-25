984d821e672f84cc2170ab48ba3d9730dad6e6a6
Gonzalez32 <3232rose@gmail.com>
2022-01-25 17:39:12 -0600

    modified statements

--- Lesson_2/statements.rb
+++ Lesson_2/statements.rb
@@ -7,9 +7,27 @@
     the if code is carried out. Otherwise, the code is ignored.
 =end
 
-a = 42
+# a = 42
 
-if a > 7 
-    puts "Yes"
+# if a > 7 
+#     puts "Yes"
+# end
+
+# if Statements
+
+=begin
+    if expressions can be nested, one inside the other.
+    This means that the inner if is the code part of the outer one. 
+    This is one way to see whether multiple conditions are satisfied. 
+    Keep in mind that once an if condition fails, 
+    the entire block is exited.
+=end
+
+num = 16
+if num > 7
+    puts "Greater than 7"
+    if num < 42
+        puts "Between 7 and 42"
+    end
 end
 
