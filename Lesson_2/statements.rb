1021706f260c166dbd0b7680ff4dd12dc52f79a5
Gonzalez32 <3232rose@gmail.com>
2022-01-25 18:27:51 -0600

    modified statements practice

--- Lesson_2/statements.rb
+++ Lesson_2/statements.rb
@@ -23,11 +23,37 @@
     the entire block is exited.
 =end
 
-num = 16
-if num > 7
-    puts "Greater than 7"
-    if num < 42
-        puts "Between 7 and 42"
+# num = 16
+# if num > 7
+#     puts "Greater than 7"
+#     if num < 42
+#         puts "Between 7 and 42"
+#     end
+# end
+# puts "--------------"
+
+# num = 8 
+# if num > 4
+#     puts "4"
+#     if num < 7
+#         puts "7"
+#         if num == 8 
+#             puts "8"
+#         end
+#     end
+# end
+
+
+puts "---------------"
+
+soccer_balls = 10
+
+if soccer_balls > 5
+    puts "There is about #{soccer_balls} soccer balls."
+    if soccer_balls < 7 
+        puts "7 indeed mate!"
+        if soccer_balls == 8
+            puts "Where is the soccer balls!"
+        end
     end
 end
-
