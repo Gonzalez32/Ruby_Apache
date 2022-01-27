365a6b00a98c1b3e25381d493ea941c5844c9536
Gonzalez32 <3232rose@gmail.com>
2022-01-26 18:33:36 -0600

    added loops

---
+++ Lesson_2/Loops.rb
@@ -0,0 +1,15 @@
+# Loops
+
+=begin
+    Loops are used to execute the same block of code a specified number of times.
+    The while loop executes a block of code while its condition is true.
+    For example:
+=end
+
+x = 0
+
+while x < 100
+    puts x
+    x += 1
+end
+
