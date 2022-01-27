0f2e6039da3c7703258c0b5e9e38ce18155d69e1
Gonzalez32 <3232rose@gmail.com>
2022-01-26 21:17:05 -0600

    modified loops

--- Lesson_2/Loops.rb
+++ Lesson_2/Loops.rb
@@ -8,8 +8,34 @@
 
 x = 0
 
-while x < 100
+while x < 50
     puts x
     x += 1
+end
+
+
+puts "================"
+
+# until Loops
+
+=begin
+    The until loop is the opposite of a while loop: 
+    it will run while its condition is false.
+=end
+
+a = 0
+
+until a > 10
+    puts "a = #{a}"
+    a += 2
+end
+
+puts "================"
+
+x = 15
+
+until x <= 10
+  puts x
+  x -= 2
 end
 
