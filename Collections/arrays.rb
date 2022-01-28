3cc64985351206bb3084c4d87a5e9644c4d569a5
Gonzalez32 <3232rose@gmail.com>
2022-01-27 22:21:14 -0600

    modified collections

---
+++ Collections/arrays.rb
@@ -0,0 +1,29 @@
+# Arrays
+
+=begin
+    An Array is essentially a list of numbered items.
+    The items are declared in a comma-separated list and
+    enclosed in square brackets.
+    
+    For example:
+=end
+
+puts "++++++++++++++++++++++"
+
+items = ["Apple", "Orange", "Banana"]
+
+=begin
+    The code above declares an array named items containing three strings.
+
+    Each item has its own unique index with the first item at index zero.
+    To refer to an item, we need to refer to its index:
+=end
+
+puts items[0]
+
+# You can assign any array element a new value by using its index:
+
+# items[1] = "Kiwi"
+
+
+
