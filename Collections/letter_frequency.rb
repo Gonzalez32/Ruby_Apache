5eb1068023787695b9c77fddcdf21f6e3bde7662
Gonzalez32 <3232rose@gmail.com>
2022-02-04 14:13:05 -0600

    letter freq setup

--- Collections/letter_frequency.rb
+++ Collections/letter_frequency.rb
@@ -25,3 +25,54 @@
     meaning that any key that does not have a value assigned will be 
     set to that value.
 =end 
+
+=begin
+    Next, we will need to iterate over each character in 
+    the string and calculate the number of occurrences in the hash. 
+    We can do that using the each_char iterator:
+
+    text.each_char { |char| freqs[char] += 1}
+
+    During each iteration, the char variable is assigned the corresponding character in our text string and 
+    then the value of that character's frequency is incremented in the freqs hash.
+    So, for example, if the letter "c" appears twice in the text, freqs["c"] will be equal to 2 after the iterator executes.
+    So, freqs will hold all the characters of the string with their corresponding occurrence number.
+
+    To show a nice result output in an alphabetical order, 
+    we can create a range of all letters and print their corresponding 
+    frequencies:
+
+    ("a".."z").each {|x| puts "#{x} : #{freqs[x]}" }
+
+    We do this because not all letters of the alphabet are contained 
+    in our text.
+
+=end
+
+
+#final code
+
+text = "I am learning Ruby and it is fun!"
+text.downcase!
+freqs = {}
+freqs.default = 0
+
+text.each_char { |char| freqs[char] += 1 }
+
+("a".."z").each { |x| puts "#{x} : #{freqs[x]}" }
+
+# Created a fully working letter frequency counter!
+
+
+puts "-------"
+
+
+# Letter Frequency
+# What is the output of this code?
+
+s = "some text goes here"
+c = 0
+s.each_char do |x| 
+  c += 1 if x == "e"
+end
+puts c
