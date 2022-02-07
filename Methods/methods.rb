a8fc483543bf2961a8f3226b0f8ac73d66809cb1
Gonzalez32 <3232rose@gmail.com>
2022-02-07 11:01:04 -0600

    modified methods parameters

--- Methods/methods.rb
+++ Methods/methods.rb
@@ -145,3 +145,41 @@ def sum x,y
 end
 
 sum 6,9
+
+# This leads to more fluid reading of code, but sometimes it can be confusing.
+
+puts "========="
+
+=begin
+    Let's create a program that prompts the user to enter a name and
+    then outputs a greeting based on the input.
+=end
+
+def greet(name="")
+    if name == ""
+        puts "Greetings!"
+    else
+        puts "Welcome, #{name}"
+    end
+end
+
+# greet(gets.chomp)
+
+=begin
+    We defined a method called greet that takes one parameter and 
+    outputs a message based on that parameter.
+    Then we called the greet method passing user input as the argument.
+
+
+    Reminder: gets.chomp is used to take user input and 
+    strip the newline at the end of the input.
+=end
+
+# Quiz
+
+def demo x=4, y=2
+    puts x**y
+end
+
+demo 5
+
