5fbc19e14a63a3ebcefb5de28b999bc23962556a
Gonzalez32 <3232rose@gmail.com>
2022-02-06 21:39:12 -0600

    modified methods pass two arguments

--- Methods/methods.rb
+++ Methods/methods.rb
@@ -100,3 +100,33 @@ y = 42
 z = 6
 
 sum(x,y,z)
+
+puts '_________'
+
+# Default Parameters
+
+=begin
+    You can also set default values for the parameters, 
+    so that the method will still work even if you do not provide all the arguments.
+    For example:   
+=end
+
+def sum(a, b=8)
+    puts a+b
+end
+
+=begin
+    Now our parameter b has a default value of 8.
+    If we pass just one argument, the method will use 
+    the value 8 as the second argument:
+=end
+
+x = 5
+sum(x)
+
+=begin
+    We can also pass two arguments as before, 
+    and the method will work as expected. 
+    The default value only comes into play when no 
+    value for that argument is provided.
+=end
