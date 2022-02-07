6d3ebfd799d59bd15c0bdee2840fb8b984e2ba67
Gonzalez32 <3232rose@gmail.com>
2022-02-07 12:22:59 -0600

    modified returning values

--- Methods/methods.rb
+++ Methods/methods.rb
@@ -216,3 +216,87 @@ end
     Therefore, you can pass two or more arguments to the method.
 =end
 
+
+puts "@@@@@"
+
+# Returning Values
+
+=begin
+    Until now all the methods we have defined output values. 
+    Sometimes you need a method to perform an action and 
+    return the result so that the returned value can be used 
+    further in the program (for example, by assigning the return 
+    value to a variable).
+    The return keyword is used to return a value from a method.
+
+    For example:
+=end
+
+def sum a, b
+    res = a+b
+    return res
+end
+
+x = sum 5, 23
+puts x
+
+y = sum 10, 10
+puts y 
+
+z = sum 30, 39
+puts z
+
+# The sum method returns the sum of its parameters, which is then assigned to variable x.
+
+puts "-----"
+
+# returning Values
+
+=begin
+    You can return multiple values from a method by separating 
+    them with commas in the return statement.
+    For example:
+=end
+
+def square(a,b,c)
+   return a*a, b*b, c*c 
+end
+
+arr = square 2, 3, 6
+puts arr
+
+puts "====="
+
+=begin
+    The result is an array containing the returned values.
+
+    Even when you do not explicitly include a return statement in your method, 
+    Ruby always returns the evaluated result of the last line of the method 
+    that is executed.
+
+    For example:
+=end
+
+def demo a, b
+    a = b-2
+    b = a-3
+end
+
+puts demo 5, 6
+
+puts demo 10, 11
+
+=begin
+    Any code in the method after a return statement will not execute, 
+    because a method stops executing once the return is executed.
+    For example:
+
+    def demo(a)
+    a = 5
+    return a
+    a = 9
+    end
+
+    The method above will return the value 5. 
+    The statement a=9 is ignored.  
+=end
