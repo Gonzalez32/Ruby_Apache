35c8eb35f934ff965c9c7319dd005633f1a2fd6f
Gonzalez32 <3232rose@gmail.com>
2022-02-07 20:03:39 -0600

    modified variable scope

--- Methods/methods.rb
+++ Methods/methods.rb
@@ -428,3 +428,32 @@ change
   
 puts x
   
+puts "----"
+
+# Global Scope
+
+=begin
+    Global variables in Ruby are accessible from anywhere in the 
+    Ruby program, regardless of where they are declared. 
+    Global variable names are prefixed with a dollar sign ($).
+
+    For example:
+=end
+
+$x = 42
+
+def change
+    $x = 8
+end
+
+change
+puts $x
+
+# As you can see the $x global variable is accessible in the whole program.
+
+=begin
+    Use of global variables is strongly discouraged. 
+    The problem with global variables is that, not only are they visible 
+    anywhere in the code, but they can also be changed from anywhere in the application. 
+    This can result in hard to find bugs.
+=end
