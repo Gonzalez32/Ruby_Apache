d8e085bb1d164b3096723b1b5f8c1798185c2cad
Gonzalez32 <3232rose@gmail.com>
2022-02-12 13:21:33 -0600

    modified oop class methods and variables

--- OOP/oop.rb
+++ OOP/oop.rb
@@ -403,3 +403,31 @@ p.show_info
     For example, if you have a variable and a method both called name, 
     self.name would make it clear that you are referring to the method.
 =end
+
+puts "------"
+
+#Class Methods
+
+=begin
+    Class methods are methods we can call directly on the class itself, 
+    without having to instantiate any objects.
+    This can be useful when there is no logical need to create an 
+    object of the class, such as when a class is used to group similar 
+    methods and functionality (like mathematical operations).
+    One example is a Math class that includes a square method for returning 
+    the square of its parameter. There is no logical need to create 
+    an object of the Math class just to call the method. 
+    This is where class methods come into play.
+    Class methods are defined using the self keyword:
+=end
+
+class Person
+    def self.info
+        puts "A Person"
+    end
+end
+
+# Now, the method is a class method and can be called directly from the class, without the need of an object:
+
+Person.info
+
