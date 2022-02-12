14918cb6e2c62e652fe5abccc7bfc79e0909ab4a
Gonzalez32 <3232rose@gmail.com>
2022-02-12 13:59:58 -0600

    modified oop class constants

--- OOP/oop.rb
+++ OOP/oop.rb
@@ -471,3 +471,22 @@ puts Person.get_count
 =end
 
 puts "@@@@@"
+
+# Class Constants
+
+=begin
+    A class can also contain constants. Remember, constant variables 
+    do not change their value and start with a capital letter. 
+    It is common to have uppercase names for constants, as in:
+=end
+
+class Calc
+    PI = 3.14
+end
+
+# You can access constants using the class name, followed by two colon symbols(::) and the constant name, for example:
+
+puts Calc::PI
+
+puts "********"
+
