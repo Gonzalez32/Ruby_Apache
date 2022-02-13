46b4e536acfa50d796d9b1e069307e752cfee7bf
Gonzalez32 <3232rose@gmail.com>
2022-02-13 13:23:14 -0600

    modified oop inheritance

--- OOP/oop.rb
+++ OOP/oop.rb
@@ -537,3 +537,25 @@ puts "---"
     Defining the to_s method makes it easier and shorter to output the information of an object in the format needed, as opposed to defining a custom method and calling it from an object.
     When you define the to_s method you call puts on your object (puts obj), where with a custom method you have to explicitly call it from the object (puts obj.info).
 =end
+
+
+puts "%%%%%%"
+
+
+# Inheritance
+
+=begin
+    Inheritance is when a class receives, or inherits, attributes and behavior from another class. The class that is inheriting behavior is called the subclass (or derived class) and the class it inherits from is called the superclass (or base class).
+    Imagine several classes, Cat, Dog, Rabbit and so on. Although they may differ in some ways (only Dog might have the method bark), they are likely to be similar in others (all having color and name).
+    This similarity can be expressed by making them all inherit from a superclass Animal, which contains the shared functionality.
+    The < symbol is used to inherit a class from another class.
+    For example:
+=end
+
+class Dog < Animal
+    # some code
+end
+
+# In the code above, Dog is the subclass and Animal in the superclass.
+
+
