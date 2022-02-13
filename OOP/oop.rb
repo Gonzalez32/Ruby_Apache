8255acbcb0c4e7ff5e338500375f216feb4b1d8e
Gonzalez32 <3232rose@gmail.com>
2022-02-13 13:15:57 -0600

    modified oop to_s method:

--- OOP/oop.rb
+++ OOP/oop.rb
@@ -490,6 +490,8 @@ puts Calc::PI
 
 puts "********"
 
+
+
 # The to_s Method
 
 # to_s method comes built-in with all classes. It gets called when you output the object.
@@ -507,3 +509,31 @@ puts p
     By default, the to_s method prints the object's class and an encoding of the object id.
 =end
 
+puts "====="
+
+=begin
+    We can define our own to_s method for a class and add custom implementation to it.
+    For example, we can generate an informative, formatted output for our Person class:
+=end
+
+class Person
+    def initialize(n, a)
+        @name = n
+        @age = a
+    end
+    def to_s
+        "#{@name} is #{@age} years old."
+    end
+end
+
+p = Person.new("David", 28)
+puts p 
+
+puts "---"
+
+# The to_s method also gets called when the object is used as a value in a string, like "#{p}"
+
+=begin
+    Defining the to_s method makes it easier and shorter to output the information of an object in the format needed, as opposed to defining a custom method and calling it from an object.
+    When you define the to_s method you call puts on your object (puts obj), where with a custom method you have to explicitly call it from the object (puts obj.info).
+=end
