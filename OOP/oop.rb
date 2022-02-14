7310c0979c55f0ea936fee28f656a87d2ffea3f0
Gonzalez32 <3232rose@gmail.com>
2022-02-14 14:45:30 -0600

    modified oop Access Modifiers

--- OOP/oop.rb
+++ OOP/oop.rb
@@ -788,7 +788,57 @@ puts c.w
     As you can see, the + method takes one argument, which is another Shape 
     object, and returns a new Shape object with the corresponding values.
 
-    
+
     You can override almost all operators in Ruby and have any custom logic 
     defined in the corresponding method.
 =end
+
+
+puts "'''''"
+
+
+# Access Modifiers
+
+=begin
+    Until now, all the methods that we defined in our classes 
+    were publicly available, meaning that you could call those methods from outside of the class.
+
+    There are certain situations when methods should only be visible to the class. 
+    For example, imagine a Banking class with methods to calculate values for internal 
+    transactions and operations. If these methods were available outside the class, the reliability of the data could be at risk.
+    To control visibility, Ruby provides the public, private, and protected access modifiers.
+
+    By default, all class methods (except initialize) are public, meaning that they are accessible from both inside and outside of the class.
+    To make a method accessible only from inside the class, we can use the private access modifier:
+=end
+
+class Person
+    def initialize(age)
+        @age = age
+    end
+
+    def show
+        puts "#{@age} years = #{days_lived} days"
+    end
+
+    private
+    def days_lived
+        @age * 365
+    end
+
+end
+
+p = Person.new(42)
+p.show
+
+=begin
+    In the code above, the method days_lived is private and is only 
+    accessible inside the class. We called it from the show method, which is public.
+    If we try to call the days_lived method from an object (puts p.days_lived), we will get an error.
+
+
+    When the reserved word private is used in a program, anything below it in the class is private 
+    (unless public or protected is placed after it to negate it).
+    Access modifiers can be applied only to methods. Instance variable are always private.
+=end
+
