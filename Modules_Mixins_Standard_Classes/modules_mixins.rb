9da8a1e830d3fcbf8173ac4ea3690e733521c753
Gonzalez32 <3232rose@gmail.com>
2022-02-17 16:57:46 -0600

    modified mixins lesson

--- Modules_Mixins_Standard_Classes/modules_mixins.rb
+++ Modules_Mixins_Standard_Classes/modules_mixins.rb
@@ -95,3 +95,40 @@ puts "0000000"
     Mixins give you a great, controlled way of adding functionality to classes.
 =end
 
+
+# Mixins
+
+=begin
+    The true power of mixins comes out when the code in the mixin starts to interact with code 
+    in the class that uses it. Ruby has a number of predefined mixins ready for you to use.
+    Let's take the standard Ruby mixin Comparable as an example. The Comparable mixin 
+    can be used to add the comparison operators (<, <=, ==, >=, and >) to a class.
+    For this to work, Comparable assumes that any class that uses it defines the operator <=>.
+    So, as a class writer, you define the one method, <=>, 
+    include Comparable, and get six comparison functions as a result!
+    Let's try this with our Cat class, by making the cats comparable based on their age.
+    All we have to do is include the Comparable module and implement the comparison 
+    operator <=>.
+=end
+
+class Cat
+    attr_accessor :name, :age
+include Comparable
+    def initialize(name, age)
+        self.name = name
+        self.age = age
+    end
+
+    def <=>(other)
+        self.age <=> other.age
+    end
+end
+
+c1 = Cat.new("Thick Boy", 4)
+c2 = Cat.new("Jimmy", 7)
+c3 = Cat.new("Lucy", 15)
+
+puts c1 < c2
+
+# Comparable module allows you to easily add comparsion operators based on any custom logic to your classes.
+
