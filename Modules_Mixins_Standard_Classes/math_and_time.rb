40ab6bceedcd3db2c8ba9937d5bde9d09fc75d2c
Gonzalez32 <3232rose@gmail.com>
2022-02-21 08:45:12 -0600

    added math and time file

---
+++ Modules_Mixins_Standard_Classes/math_and_time.rb
@@ -0,0 +1,26 @@
+# Standard Classes
+
+=begin
+    Ruby provides a number of standard built-in classes that make our life a lot easier 
+    by providing useful methods for manipulating data.
+    Some of the classes in previous lessons include Array, String and Struct.
+    Another useful class is the Math class, which provides methods to perform 
+    mathematical operations.
+    For example:
+=end
+
+# square root
+puts Math.sqrt(9)
+
+puts "------"
+# pi constant
+puts Math::PI
+
+puts "------"
+# trigonometry (sin, cos, tan)
+puts Math::cos(0)
+
+puts "------"
+
+# Run the code and see how it work!
+
